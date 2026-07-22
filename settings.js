20183fa4e2f4ee10c78945852995dc1725d0d19d
YourName <your@email.com>
2026-07-22 08:58:39 +0200

    Fix owner and admin detection

--- settings.js
+++ settings.js
@@ -1,19 +1,17 @@
+global.ownerNumber = "263716616101";
+global.ownerName = "Kelly";
+global.botName = "TechX-MD";
+global.prefix = ".";
+global.sessionName = "session";
 
-global.owner = "263716616101"
-global.ownerName = "Kelly"
-global.botName = "TechX-MD"
-global.prefix = "."
-global.sessionName = "session"
+global.autoRead = false;
+global.autoTyping = false;
+global.autoRecording = false;
+global.autoOnline = true;
+global.autoReact = true;
+global.reactEmoji = "🔥";
 
-global.autoRead = false
-global.autoTyping = false
-global.autoRecording = false
-global.autoOnline = true
-global.autoReact = true
-global.reactEmoji = "🔥"
+global.packname = "TechX-MD";
+global.author = "Kelly";
 
-
-global.packname = "TechX-MD"
-global.author = "Kelly"
-
-module.exports = global
+module.exports = global;
